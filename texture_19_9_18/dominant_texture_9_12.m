6abeb28ce6b1c492fa607a4df26e3c28fe3854af
wang shengchun <1293294621@qq.com>
2019-09-27 18:36:41 +0800

    2019.9.27 源代码中添加一次梯度、二次梯度数据统计，即相同梯度值出现次数

--- texture_19_9_18/dominant_texture_9_12.m
+++ texture_19_9_18/dominant_texture_9_12.m
@@ -9,7 +9,7 @@ addpath(genpath(pwd));
 str=[FilePath FileName];
 img_rgb=imread(str);
 %% Convert the RGB image to HSI image
-img_gray=im2double(rgb2gray(img_rgb));
+%img_gray=im2double(rgb2gray(img_rgb));
 img_gray=rgb2gray(img_rgb);
 img_hsi=rgb2hsi(img_rgb);
 data_I=img_hsi(:,:,3);
@@ -18,14 +18,41 @@ data_H=img_hsi(:,:,1);
 Hue_data=data_H;
 Intensity_data=data_I;
 Saturation_data=data_S;
+%% PCA 
+gray_pca=double(img_gray);
+stdr=std(gray_pca);                       %求各变量的标准差；
+[n,m]=size(gray_pca);               %矩阵的行与列
+sddata=gray_pca./stdr(ones(n,1),:);         %标准化变换
+[p,princ,egenvalue]=princomp(sddata);  %调用主成分
+p=p(:,1:3);                          %输出前3主成分系数；
+sc=princ(:,1:3);                       %前3主成分量；
+egenvalue;                              %相关系数矩阵的特征值，即各主成分所占比例；
+per=100*egenvalue/sum(egenvalue);       %各个主成分所占百分比；
+%[coef,score,latent,t2] = princomp(im2double(img_gray))
 %%
 temp_img_gray=img_gray;
 temp_img_gray=im2double(temp_img_gray);
 [out_row_gray,out_colum_gray,out_final_gray,out_eight_final]=Gradient_calculation(temp_img_gray);%Calculate the gradient out_final_gray:四邻域计算;out_eight_final:八邻域计算
-temp_abs_gray_gradient=im2uint8(out_final_gray);
+temp_abs_gray_gradient=im2uint8(out_eight_final);
 figure
 imshow(temp_abs_gray_gradient);
 title('gradient image');
+%%
+%二次梯度
+[out_row_gray2,out_colum_gray2,out_final_gray2,out_eight_final2]=Gradient_calculation(out_eight_final);%Calculate the gradient out_final_gray:四邻域计算;out_eight_final:八邻域计算
+temp_abs_gray_gradient2=im2uint8(out_eight_final2);
+
+tbl=tabulate(temp_abs_gray_gradient(:));
+set(0,'defaultfigurecolor','w')
+plot(tbl(:,1)',tbl(:,2)','b');%蓝色
+hold on
+tbl2=tabulate(temp_abs_gray_gradient2(:));
+plot(tbl2(:,1)',tbl2(:,2)','r');%红色
+legend('一次梯度','二次梯度');
+xlabel('梯度值');
+ylabel('同梯度总数');
+
+%%
 temp_abs_gray_gradient(find(temp_abs_gray_gradient<40))=0;%Modify the threshold to get a different gradient map
 temp_vary_abs_gray_gradient=temp_abs_gray_gradient;
 temp_vary_abs_gray_gradient(find(temp_vary_abs_gray_gradient>0))=255;
@@ -52,3 +79,7 @@ title('边界')
 % imshow(Saturation_data);
 % title('Saturation image');
 % [row_x,col_y]=size(Intensity_data);
+
+%%
+latent=100*latent/sum(latent)%将latent总和统一为100，便于观察贡献率
+pareto(latent);%调用matla画图
